7a9bd0e03ef4402f7c637bd2c22959a8754153a7
User <User@DESKTOP-CUANQ1J.fritz.box>
2018-12-20 21:23:14 +0100

    fix

--- Project_BBP/src/database/Provider.java
+++ Project_BBP/src/database/Provider.java
@@ -3,7 +3,7 @@ package database;
 public interface Provider {
 	String DRIVER = "com.mysql.jdbc.Driver";
 	String CONNECTION_URL = "jdbc:mysql://server2.febas.net/InDon";
-	String USERNAME = "InDonAdmin";
-	String PASSWORD = "InDon123";
+	String USERNAME = "bbp-admin";
+	String PASSWORD = "BBPJavaProject";
 
 }
