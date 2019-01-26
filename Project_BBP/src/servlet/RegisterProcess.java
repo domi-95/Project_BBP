eba0d75add8a403a2e4f7e22c1847ad536db2615
kevi_ <kevi_@DESKTOP-P61UEMV.fritz.box>
2019-01-26 15:24:10 +0100

    registerprocess release 2

--- Project_BBP/src/servlet/RegisterProcess.java
+++ Project_BBP/src/servlet/RegisterProcess.java
@@ -18,112 +18,113 @@ import user.User;
 @WebServlet("/register")
 public class RegisterProcess extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RegisterProcess() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public RegisterProcess() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		HttpSession session = request.getSession();
+		
+		//get session user if exist
 		User u = null;
-		if((User)session.getAttribute("user") != null){
-			u = (User)session.getAttribute("user"); 
+		if ((User) session.getAttribute("user") != null) {
+			u = (User) session.getAttribute("user");
 		}
 		
-			String firstname = request.getParameter("firstname");
-			String name = request.getParameter("name");
-    		String email = request.getParameter("email");
-    		String password1 = request.getParameter("password1");
-    		String password2 = request.getParameter("password2");
-    		String message  = null;
-    		
-    		
-    		int role_id = 1;
-    		if(u != null && u.getRole().getId() == 2 && request.getParameter("usercategory") != null){
-    			role_id = Integer.parseInt(request.getParameter("usercategory"));
-    		}
-    		//out.print(email + password1);
-    		
-    		//Check if all fields are filled
+		//get parameters from form
+
+		String firstname = request.getParameter("firstname");
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String password1 = request.getParameter("password1");
+		String password2 = request.getParameter("password2");
+		String message = null;
+
+		// default for citizen
+		int role_id = 1;
+
+		// check if administration register an user
+		if (u != null && u.getRole().getId() == 2 && request.getParameter("usercategory") != null) {
+			role_id = Integer.parseInt(request.getParameter("usercategory"));
+		}
+
+		// Check if all fields are filled
 		if (firstname != null && name != null && email != null && password1 != null && password2 != null) {
 
-			//out.print(firstname + email + password1);
-			
+			// get user from db
 			User user = UserDao.searchUser(email);
-			//Check if user exists
-			if(user != null){
-				if(email.equals(user.getEmail())){
+			// Check if user exists
+			if (user != null) {
+				if (email.equals(user.getEmail())) {
 					message = "Es gibt bereits einen Account mit der gleichen E-Mail Adresse";
 					request.setAttribute("message", message);
 					request.getRequestDispatcher("register.jsp").forward(request, response);
 					System.out.println("Sorry, you are already registered");
 				}
-			}
-			else{
-				//Check if passwords match
-				if(!password1.equals(password2) ){
+			} else {
+				// Check if passwords match
+				if (!password1.equals(password2)) {
 					message = "Passwörter stimmen nicht überein" + password1 + password2;
 					request.setAttribute("message", message);
 					request.getRequestDispatcher("register.jsp").forward(request, response);
 				}
-				//Safe User and forward to index
+				// Safe User and forward to index
 				else {
 					if (User.createUser(email, name, firstname, password1, role_id)) {
-						
-					
-					//UserDao.safeUser(email, name, firstname, password1, role_id);
-					User n = User.getUser(email);
-					//User n = UserDao.searchUser(email);
-					
+						User n = User.getUser(email);
+
 						if (n.getRole().getId() == 1) {
-							if(u == null){
+							if (u == null) {
 								session.setAttribute("user", n);
 								message = "Sie haben sich erfolgreich registriert und werden angemeldet!";
+								getServletContext().getRequestDispatcher("/message.jsp").forward(request, response);
+								return;
 							}
-							if (u != null ) {
-								
+							if (u != null) {
+
 								message = "Sie haben sich erfolgreich einen Bürger registriert!";
 							}
-						//}
 						}
 						if (n.getRole().getId() == 2) {
-							//response.sendRedirect("index.jsp");
+							// response.sendRedirect("index.jsp");
 							message = "Sie haben erfolgreich ein Verwaltungsmitglied registriert!";
 						}
 						if (n.getRole().getId() == 3) {
-							//response.sendRedirect("index.jsp");
+							// response.sendRedirect("index.jsp");
 							message = "Sie haben erfolgreich ein Gemeinderatsmitglied registriert!";
 						}
 						// sets the message in request scope
-						request.setAttribute("Message", message);
-						getServletContext().getRequestDispatcher("/message.jsp").forward(request, response);
+						request.setAttribute("message", message);
+						getServletContext().getRequestDispatcher("/register.jsp").forward(request, response);
 
-						
-					}else {
+					} else {
 						message = "Fehler beim registrieren!";
-						request.setAttribute("Message", message);
-						getServletContext().getRequestDispatcher("/message.jsp").forward(request, response);
+						request.setAttribute("message", message);
+						getServletContext().getRequestDispatcher("/register.jsp").forward(request, response);
 
 					}
 				}
-				}
+			}
 		}
-    		
-	
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
