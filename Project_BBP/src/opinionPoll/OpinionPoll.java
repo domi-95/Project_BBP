8324abdac5e097d38f5a66b11b165ae4c485b947
kevi_ <kevi_@DESKTOP-P61UEMV.fritz.box>
2019-02-04 23:30:11 +0100

    fix bug

--- Project_BBP/src/opinionPoll/OpinionPoll.java
+++ Project_BBP/src/opinionPoll/OpinionPoll.java
@@ -89,13 +89,13 @@ public class OpinionPoll {
 
 	public String getDate_from() {
 		DateFormat f = new SimpleDateFormat("dd.MM.yyyy' 'HH:mm 'Uhr'"); // timstamp converted to readable string
-		return f.format(created);
+		return f.format(date_from);
 
 	}
 
 	public String getDate_to() {
 		DateFormat f = new SimpleDateFormat("dd.MM.yyyy' 'HH:mm 'Uhr'"); // timstamp converted to readable string
-		return f.format(created);
+		return f.format(date_to);
 
 	}
 
